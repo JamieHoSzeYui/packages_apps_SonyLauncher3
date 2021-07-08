91a512e3c6c5a49961ace38707bd27e9b1d7aa9e
Wiley Lau <henloboii@protonmail.ch>
2021-07-08 16:38:56 +0800

    SonyLauncher3: Initial files push
    Co-authored-by: Herobuxx <me@herobuxx.xyz>
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

---
+++ launcher.mk
@@ -0,0 +1,9 @@
+PRODUCT_PACKAGES += \
+    XperiaHome
+
+# Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    package/apps/SonyLauncher3/overlay/common
+
+DEVICE_PACKAGE_OVERLAYS += \
+    packages/apps/SonyLauncher3/overlay/common
